abb28079cd5fce0335d2a39d3b7f6146c15cfe08
Henry Liu <henry.liu@salesforce.com>
2017-10-27 14:44:09 -0700

    update for github

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var express = require('express'),
 	http = require('http').Server(app),
 	jsforce = require('jsforce'),
 	bodyParser = require('body-parser');
-	//yaml = require('js-yaml');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+//connect to salesforce
 var sfConn = new jsforce.Connection({});
-sfConn.login("henry@dreamforce.iot", "test1234", function(err, userInfo){
+sfConn.login("<salesforce username>", "<password>", function(err, userInfo){
 	if (err) { return console.error(err); }
 	console.log('logged into salesforce');
 });
@@ -23,19 +23,18 @@ app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+//post to fridge event with the json provided in request
 app.post('/FridgeEvent', function(req, res, next){
 	console.log('raw req body:');
 	console.log(req.body);
 	console.log('json stringify body:');
 	console.log(JSON.stringify(req.body));
-
+	//create salesforce record
 	sfConn.sobject("Fridge_Event__e").create({Serial_Number__c : req.body.Serial_Number__c, Temperature__c : req.body.Temperature__c}, function(err, result){
 		if(err) 
 			return console.error(err);
 		console.log("Created record id : " + result.id);
 	});
-	
-	
 
 	res.status(200).end();
 });
